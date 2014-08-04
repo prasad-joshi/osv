2fe4d885d82a0dddc0a9bba7cd5f0acb35252176
Prasad Joshi <prasadjoshi.linux@gmail.com>
2014-08-04 16:58:15 +0530

    vfs: remove unnecessary while loop
    Signed-off-by: Prasad Joshi <prasadjoshi.linux@gmail.com>

--- fs/vfs/main.cc
+++ fs/vfs/main.cc
@@ -2099,7 +2099,7 @@ extern "C" void mount_zfs_rootfs(void)
         if (ret) {
             printf("failed to mount %s, error = %s\n", m->mnt_type, strerror(ret));
         }
-    } while (m != nullptr);
+    }
     endmntent(ent);
 
     import_extra_zfs_pools();
